d6158e6cb71d7adee0aac251983adcf2ffc570c8
Robin Southern <betajaen@ihoed.com>
2014-09-08 20:16:31 +0100

    [Add] Basic transform for OkSurface

--- Okapi/OkDrawing.cs
+++ Okapi/OkDrawing.cs
@@ -8,6 +8,10 @@ namespace Okapi
 
     private Mesh mMesh;
     private Material mMaterial;
+    private Vector2 mOffsetPosition;
+    private float mOffsetScale;
+    private float mOffsetRotation;
+
     private Vector3 mPosition, mScale;
     private Quaternion mRotation;
     private Matrix4x4 mTransform;
@@ -20,7 +24,56 @@ namespace Okapi
 
     public void Destroy()
     {
+    }
 
+    public Vector2 position
+    {
+      get { return mOffsetPosition; }
+      set
+      {
+        mOffsetPosition = value;
+        RefreshTransform();
+      }
+    }
+
+    public float scale
+    {
+      get { return mOffsetScale; }
+      set
+      {
+        mOffsetScale = Mathf.Abs(value);
+        RefreshTransform();
+      }
+    }
+
+    public float rotation
+    {
+      get { return mOffsetRotation; }
+      set
+      {
+        mOffsetRotation = value;
+        RefreshTransform();
+      }
+    }
+
+    void RefreshTransform()
+    {
+      mScale.x = mOffsetScale;
+      mScale.y = -mOffsetScale;
+      mScale.z = 0.0f;
+
+      mPosition.x = mOffsetPosition.x;
+      mPosition.y = mOffsetPosition.y;
+
+      mRotation = Quaternion.AngleAxis(mOffsetRotation, new Vector3(0, 0, 1.0f));
+
+      mTransform = Matrix4x4.TRS(mPosition, mRotation, mScale);
+    }
+
+    public void Draw()
+    {
+      mMaterial.SetPass(0);
+      Graphics.DrawMeshNow(mMesh, mTransform);
     }
 
   }
